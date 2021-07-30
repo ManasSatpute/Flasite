dd633d6fd5eec50a7faeb13372f98021b6271fa6
Manas Satpute <manas.satpute@gmail.com>
2021-07-31 03:28:01 +0530

    Initialization

---
+++ flasite/__init__.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
+
+mail = Mail(app)
+
+from flasite.users.routes import users
+from flasite.posts.routes import posts
+from flasite.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
---
+++ flasite/users/utils.py
@@ -0,0 +1,33 @@
+import os
+import secrets
+from PIL import Image
+from flask import url_for, render_template
+from flask_mail import Message
+from flasite import app, mail
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='manas.satpute99@gmail.com',
+                  recipients=[user.email])
+    url=url_for('users.reset_token', token=token, _external=True)
+    msg.body = f'''
+    '''
+    msg.html=render_template('email_reset.html' ,url=url,email=user.email)
+
+    mail.send(msg)
