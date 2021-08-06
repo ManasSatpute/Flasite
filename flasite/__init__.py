bd4529099df374a0602ff9b7f2cac4fbe60c9fed
Manas Satpute <manas.satpute@gmail.com>
2021-08-07 03:44:16 +0530

    comit

--- flasite/__init__.py
+++ flasite/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bekttykzfmadzf:1c448c728cd8fd296ef5241ac92785e81e2890c81671665243237f67b7d09d75@ec2-3-226-59-11.compute-1.amazonaws.com:5432/d4l812t330k3bp'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bekttykzfmadzf:1c448c728cd8fd296ef5241ac92785e81e2890c81671665243237f67b7d09d75@ec2-3-226-59-11.compute-1.amazonaws.com:5432/d4l812t330k3bp'
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USE_SSL'] = True
