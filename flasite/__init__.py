ea381c530eee2cac1e736209fc58bb8904733c9a
Manas Satpute <manas.satpute@gmail.com>
2021-08-08 02:34:25 +0530

    comit

--- flasite/__init__.py
+++ flasite/__init__.py
@@ -7,7 +7,6 @@ from flask_mail import Mail
 from flask_login import LoginManager
 
 from os.path import join, dirname
-from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
