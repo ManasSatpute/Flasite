fb5f79b59bcaefd03165a82f3ec1f628d5b6be34
Manas Satpute <manas.satpute@gmail.com>
2021-08-07 19:24:26 +0530

    done

--- flasite/__init__.py
+++ flasite/__init__.py
@@ -17,13 +17,13 @@ mail = Mail()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USE_SSL'] = True
-    app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-    app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+    app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
+    app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
 
     db.init_app(app)
     bcrypt.init_app(app)
